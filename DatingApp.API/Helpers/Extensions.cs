523bf681cdd9a906574de2c67e3f0121fd8feac2
Spencer Du <spencer.du@embl.de>
2020-05-07 03:27:10 +0200

    End of section 8

--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Http;
 
 namespace DatingApp.API.Helpers
@@ -9,6 +10,15 @@ namespace DatingApp.API.Helpers
             response.Headers.Add("Application-Error", message);
             response.Headers.Add("Access-Control-Expose-Header", "Application-Error");
             response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+
+        public static int CalculateAge(this DateTime theDateTime)
+        {
+            var age = theDateTime.Today.Year - theDateTime.Year;
+            if (theDateTime.AddYears(age) > theDateTime.Today)
+                age--;
+
+            return age;
         }
     }
 }
